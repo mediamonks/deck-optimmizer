515d6969de1787b61e3b58cde17be444920adca7
Mirko <mirko@mediamonks.com>
2022-04-22 15:01:36 +0200

    updated files

--- src/util/copyGoogleSlides.js
+++ src/util/copyGoogleSlides.js
@@ -1,24 +1,57 @@
 const {google} = require("googleapis");
 
-module.exports = async function copyGoogleSlides(sourcePresentationId, oAuth2Client) {
-    const slidesService = google.slides({version: 'v1', auth: oAuth2Client});
-    const driveService = google.drive({version: 'v3', auth: oAuth2Client});
-
-    const originalPresentation = await slidesService.presentations.get({presentationId: sourcePresentationId});
-
-    const newPresentationId =  await new Promise((resolve) => {
-        driveService.files.copy({
-            fileId: originalPresentation.data.presentationId,
+const getDriveFilePermissions = async (fileId, driveService) => {
+    return await new Promise((resolve) => {
+        driveService.permissions.list({
+            fileId: fileId,
             supportsAllDrives: true,
+            fields: '*'
+        }, (err, driveResponse) => {
+            if (err) console.log(err)
+            resolve(driveResponse.data);
+        });
+    });
+}
+
+const copyDriveFile = async (fileId, newName, driveService) => {
+    return await new Promise((resolve) => {
+        driveService.files.copy({
+            fileId: fileId,
+            supportsAllDrives: true,
+            includePermissionsForView: 'published',
             resource: {
-                name: 'Optimized Copy of ' + originalPresentation.data.title,
+                name: newName,
             },
         }, (err, driveResponse) => {
             if (err) console.log(err)
-            resolve(driveResponse.data.id);
-
+            resolve(driveResponse.data);
         });
     })
+}
 
-    return await slidesService.presentations.get({ presentationId: newPresentationId });
+const createDriveFilePermissions = async (fileId, request, driveService) => {
+    return await new Promise((resolve) => {
+        driveService.permissions.create({
+            fileId: fileId,
+            supportsAllDrives: true,
+            resource: request,
+        }, (err, driveResponse) => {
+            if (err) console.log(err)
+            resolve(driveResponse.data);
+        });
+    })
+}
+
+module.exports = async function copyGoogleSlides(sourcePresentationId, auth) {
+    const slidesService = google.slides({version: 'v1', auth});
+    const driveService = google.drive({version: 'v3', auth});
+
+    const originalPresentation = await slidesService.presentations.get({presentationId: sourcePresentationId});
+
+    const newPresentation = await copyDriveFile(sourcePresentationId, 'Optimized Copy of ' + originalPresentation.data.title, driveService);
+
+    const permission = { type: 'anyone', kind: 'drive#permission', role: 'writer' }
+    await createDriveFilePermissions(newPresentation.id, permission, driveService);
+
+    return await slidesService.presentations.get({ presentationId: newPresentation.id });
 }
