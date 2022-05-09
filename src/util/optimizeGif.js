edbe0183d8d0956e19ccd1d6814be46d82c7c679
Mirko <mirko@mediamonks.com>
2022-05-09 13:59:24 +0200

    apply cropping automatically on gifs when needed

--- src/util/optimizeGif.js
+++ src/util/optimizeGif.js
@@ -1,12 +1,27 @@
 const {execFile} = require("child_process");
 const gifsicle = require("gifsicle");
 
-module.exports = async function optimizeGif( sourceImagePath, outputImagePath, optimizeLevel = 30 ) {
+module.exports = async function optimizeGif( sourceImagePath, outputImagePath, optimizeLevel = 30 , cropLine = '') {
+    // http://www.lcdf.org/gifsicle/man.html
+
+    const optimizationArray = [];
+
+    console.log(cropLine)
+
+    // apply desired level of lossiness
+    optimizationArray.push('--lossy='+optimizeLevel.toString());
+
+    // apply desired crop
+    optimizationArray.push('--crop='+cropLine.toString());
+
+    // TO DO apply desired FPS
+
     return new Promise((resolve) => {
-        execFile(gifsicle, [ // http://www.lcdf.org/gifsicle/man.html
-            '--lossy='+optimizeLevel.toString(),
+        execFile(gifsicle, [
+            ...optimizationArray,
             '-o', outputImagePath,
-            sourceImagePath], (error, stdout) => {
+            sourceImagePath
+        ], (error, stdout) => {
 
             if (error) {
                 throw error;
