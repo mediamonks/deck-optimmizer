cdb9c933e2e74a3645e97c536679e978df835a9c
pavel b <pavel.belitskiy@gmail.com>
2022-08-11 17:54:58 +0200

    Updated directory structure

---
+++ source/node/util/optimizeGif.js
@@ -0,0 +1,32 @@
+const {execFile} = require("child_process");
+const gifsicle = require("gifsicle");
+
+module.exports = async function optimizeGif( sourceImagePath, outputImagePath, applyLossy, factor, applyColourCorrect, colourRange) {
+    // http://www.lcdf.org/gifsicle/man.html
+
+    const optimizationArray = [];
+
+    if (applyLossy) {
+        // apply desired level of lossiness
+        optimizationArray.push('--lossy='+factor.toString());
+    };
+    
+    if (applyColourCorrect) {
+        // apply color correction
+        optimizationArray.push('--colors='+colourRange.toString());
+    };
+
+    return new Promise((resolve) => {
+        try{
+            execFile(gifsicle, [
+                ...optimizationArray,
+                '-o', outputImagePath,
+                sourceImagePath
+            ], (error, stdout) => {
+            resolve(stdout);
+            });
+        } catch (error) {
+            resolve();
+        };
+    });
+}
