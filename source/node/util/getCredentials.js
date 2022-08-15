26dafec3a0aacf944dde14c33bb796eca8416413
pavel b <pavel.belitskiy@gmail.com>
2022-08-15 11:59:10 +0200

    Added retrieve credentials from env

--- source/node/util/getCredentials.js
+++ source/node/util/getCredentials.js
@@ -1,53 +1,28 @@
 const fs = require("fs-extra");
 const inquirer = require("inquirer");
+const dotenv = require('dotenv');
+
 module.exports = async function getCredentials( credsFilePath ) {
     let credentials;
 
     try {
         credentials = await fs.readJson(credsFilePath);
     } catch (e) {
-        const google_questions = ['client_email', 'private_key', 'client_id'].map(question => {
-            return {
-                type: 'input',
-                name: question,
-                message: question
-            }
-        })
-
-        const google_answers = await inquirer.prompt(google_questions);
-
-        const aws_questions = ['accessKeyId', 'secretAccessKey', 'bucket'].map(question => {
-            return {
-                type: 'input',
-                name: question,
-                message: question
-            }
-        })
-
-        const aws_answers = await inquirer.prompt(aws_questions);
-
-        const workato_questions = ['client_id', 'client_secret'].map(question => {
-            return {
-                type: 'input',
-                name: question,
-                message: question
-            }
-        })
-
-        const workato_answers = await inquirer.prompt(workato_questions);
-
+        await dotenv.config();
         credentials = {
             'google': {
-                client_email: google_answers.client_email,
-                private_key: '-----BEGIN PRIVATE KEY-----\n'+google_answers.private_key+'\n-----END PRIVATE KEY-----',
-                client_id: google_answers.client_id
+                client_email: process.env.CLIENT_EMAIL,
+                private_key: '-----BEGIN PRIVATE KEY-----\n'+process.env.PRIVATE_KEY+'\n-----END PRIVATE KEY-----',
+                client_id: process.env.CLIENT_ID
             },
             'aws': {
-                ...aws_answers
+                accessKeyId: process.env.ACCESSKEYID,
+                secretAccessKey: process.env.SECRETACCESSKEY,
+                bucket: process.env.BUCKET
             },
             'workato': {
-                client_id: workato_answers.client_id,
-                client_secret: workato_answers.client_secret
+                client_id: process.env.WORKATO_CLIENT_ID,
+                client_secret: process.env.WORKATO_CLIENT_SECRET
             }
         }
         await fs.writeJson(credsFilePath, credentials);
